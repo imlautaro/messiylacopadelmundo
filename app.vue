1dab627cac1a7a6bd9cf0a65d29820dd5e81472d
Lautaro Pereyra <hi@imlautaro.com>
2022-12-19 10:56:52 -0300

    initial commit

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="container">
+		<img src="/messi.jpg" />
+	</div>
+</template>
+
+<style>
+body {
+	margin: 0;
+}
+.container {
+	min-height: 100vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+.container img {
+	width: 100%;
+	max-width: 32rem;
+	border-radius: 1em;
+}
+</style>
